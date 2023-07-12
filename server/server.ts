f9eb3bc377da019ff271fc02f20d315a052e9e5c
A Holly W Sphar <ninohero@muzocalo.com>
2023-07-12 12:20:40 -0400

    angular material course, fixed api server and made cards load from api data

--- server/server.ts
+++ server/server.ts
@@ -22,7 +22,7 @@ app.route('/api/lessons').get(searchLessons);
 
 
 const httpServer:any = app.listen(9000, () => {
-    console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port);
+    console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port + ", chappa");
 });
 
 
